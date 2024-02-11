2a882b660059e9e8e898f59ae0263633f8efb232
Dark_Imesh <79241963+DARKSL-IME@users.noreply.github.com>
2024-02-11 10:08:35 +0530

    Update

---
+++ 1.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+
+    <style>
+        body {
+            padding: 20px;
+            background-color: #4caf50; 
+            color: rgb(17, 17, 17);
+        }
+
+        .container {
+            background-color: white; 
+            padding: 20px;
+            border-radius: 8px;
+            margin-top: 20px;
+        }
+
+        .btn-custom {
+            background-color: #4caf50;
+            color: white;
+        }
+
+        .error-message {
+            color: red;
+            margin-top: 5px;
+        }
+    </style>
